5b87b63f5a4ef468500e2ec9489cf269ca15bb8b
joaoomarcc <84502775+joaoomarcc@users.noreply.github.com>
2026-07-18 19:59:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-function Home(){
-    return <h1>Se você está vendo isso é porque você é uma pessoa especial pra mim e provavelmente nunca viu algo que eu fiz. Te amo!❤️​</h1>}
-
-export default Home; 
+function Home() {
+  return (
+    <h1>
+      Se você está vendo isso é porque você é uma pessoa especial pra mim e
+      provavelmente nunca viu algo que eu fiz. Te amo!❤️​
+    </h1>
+  );
+}
+export default Home;
